c705d40f06ba5882f65a89da70a149ee8b178e3a
zakariya <zakariasabri2022@gmail.com>
2024-11-18 05:32:13 +0100

    code JS

--- projet-Array/SM.js
+++ projet-Array/SM.js
@@ -1,13 +1,20 @@
-let array =[","];
- let ele ;
- document.getElementById("submit").addEventListener("click",function(){
-    ele = document.getElementById("text").ariaValueMax; 
-    array.push("ele");
-    document.getElementById("output")=array;
- })
- document.getElementById("rest").addEventListener("click",function(){
-   if(array !== null){
+let array = [];
+let ele;
+document.getElementById("submit").addEventListener("click", function () {
+  ele = document.getElementById("text").value;
+  console.log(ele);
+  if (ele !== "") {
+    array.push(ele);
+    console.log(array);
+    document.getElementById("output").innerHTML = array;
+    document.getElementById("text").value = '';
+  }
+
+})
+document.getElementById("reset").addEventListener("click", function () {
+  if (array !== null) {
     array.pop("ele");
-    document.getElementById("output")=array;
-   }
- })
+    console.log(array);
+    document.getElementById("output").innerHTML = array;
+  }
+})
